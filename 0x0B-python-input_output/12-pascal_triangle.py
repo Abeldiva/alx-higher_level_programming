461a6b691b887dbb691ad4c19bc1850f829447a8
abeldiva <abelbenita51@gmail.com>
2024-05-18 02:24:05 -0700

    12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -11,14 +11,14 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     temp = []
-    attr = []
+    a = []
     for i in range(n):
         row = []
         for j in range(i + 1):
             if i == 0 or j == 0 or i == j:
                 row.append(1)
             else:
-                row.append(l[j] + l[j - 1])
-        attr = row
+                row.append(a[j] + l[j - 1])
+        a = row
         temp.append(row)
     return temp
