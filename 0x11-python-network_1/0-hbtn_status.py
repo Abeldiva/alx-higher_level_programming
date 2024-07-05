6fb380d06f579f2a153d2511c0617937306483c1
abeldiva <abelbenita51@gmail.com>
2024-07-05 05:18:42 -0700

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" 0-hbtn_status.py
+Python script that fetches the url and displays the body"""
+
+
+import urllib.request
+
+url = "https://alu-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    content = response.read()
+
+    print("Body response:")
+    print("\t- type:", type(content))
+    print("\t- content:", content)
+    print("\t- utf8 content:", content.decode('utf-8'))
