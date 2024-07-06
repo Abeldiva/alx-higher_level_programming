07c586e3f47c93ad2bb10e0ed0600316113f68b9
abeldiva <abelbenita51@gmail.com>
2024-07-06 05:23:19 -0700

    101-post_json.sh

---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Script that sends a POST request with the contents of a file, passed with the filename as the second argument of the script, in the body of the request.
+curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
