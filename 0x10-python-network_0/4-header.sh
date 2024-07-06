19a43e88d912948b455bbeabf49b78c1f8306b6e
abeldiva <abelbenita51@gmail.com>
2024-07-06 05:05:02 -0700

    4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in URL as an argument, sends a GET request, and displays the body of the response.
-curl "$1" -s GET -H "X-HolbertonSchool-User-Id:98"
+curl "$1" -sX GET -H "X-School-User-Id"
