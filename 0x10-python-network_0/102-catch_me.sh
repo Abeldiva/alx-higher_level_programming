ccd4b0c93bff365c4ca71d60a9e9f02a5153ddd4
abeldiva <abelbenita51@gmail.com>
2024-07-06 06:11:54 -0700

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes a request and causes the server to respond with the contained message.
-curl -sL -X PUT -d "0.0.0.0:5000/catch_me" -H "You got me!" "$1"
+curl 0.0.0.0:5000/catch_me -sL -X PUT -H "user_id=98" -d "You got me!" "$1"
